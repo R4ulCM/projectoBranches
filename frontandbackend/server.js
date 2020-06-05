eb59486a66de293b5241f070b5c5a2291c602b8a
RaulCM <carloraul.martinez@gmail.com>
2020-06-05 17:27:20 -0500

    Estructura base de json

--- frontandbackend/server.js
+++ frontandbackend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 const cors = require('cors');
 const db = require('./db.json');
+const br = require('./br.json');
 
 app.listen(port,function(){
     console.log("Inicia Servidor... con el puerto: "+port);
@@ -14,6 +15,8 @@ app.use(express.json());
 
 app.get('/api/contact',getUserGeneral);
 app.get('/api/contact/:id',getUserSingle);
+app.get('/api/branches/',getBranGeneral);
+app.get('/api/branches/:id',getBranSingle);
 
 function getUserGeneral(request, response){
     response.send(db);
@@ -26,5 +29,19 @@ function getUserSingle(request, response){
         response.status(200).send(userSelect);
     }else{
         response.status(404).send({error:'Usuario no encontrado'});
+    }
+}
+
+function getBranGeneral(request, response){
+    response.send(br);
+}
+
+function getBranSingle(request, response){
+    const id = request.params.id;
+    const branchesSelect = br.branches[id];
+    if(branchesSelect){
+        response.status(200).send(branchesSelect);
+    }else{
+        response.status(404).send({error:'No se han encontrado datos relacionados ('+id+')'});
     }
 }
